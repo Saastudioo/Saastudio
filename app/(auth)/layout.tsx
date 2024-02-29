8588b3c3ae3949819b628609cb84a9ba869b9cb9
SaastudioOfficial <161658619+SaastudioOfficial@users.noreply.github.com>
2024-02-29 14:38:01 +1300

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import Illustration from '@/public/images/auth-illustration.svg'
+// import Illustration from '@/public/images/auth-illustration.svg'
 
 export default function AuthLayout({
   children,
@@ -12,9 +12,9 @@ export default function AuthLayout({
       <section className="relative">
 
         {/* Illustration */}
-        <div className="md:block absolute left-1/2 -translate-x-1/2 -mt-36 blur-2xl opacity-70 pointer-events-none -z-10" aria-hidden="true">
+{/*         <div className="md:block absolute left-1/2 -translate-x-1/2 -mt-36 blur-2xl opacity-70 pointer-events-none -z-10" aria-hidden="true">
           <Image src={Illustration} className="max-w-none" width={1440} height={450} priority alt="Page Illustration" />
-        </div>
+        </div> */}
 
         <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
